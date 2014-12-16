c9d1fe186a4fc99b4b019ebc8d746f8462305f6a
Kurt Spindler <kurt@uber.com>
2014-12-16 15:29:22 -0800

    fix hipchat handler

--- graphite_beacon/handlers/hipchat.py
+++ graphite_beacon/handlers/hipchat.py
@@ -23,6 +23,8 @@ class HipChatHandler(AbstractHandler):
     def init_handler(self):
         self.room = self.options.get('room')
         self.key = self.options.get('key')
+        self.author = self.options.get('author', 'Graphite Beacon')
+        self.notify_levels = self.options.get('notify_levels', [])
         assert self.room, 'Hipchat room is not defined.'
         assert self.key, 'Hipchat key is not defined.'
         self.client = hc.AsyncHTTPClient()
@@ -32,15 +34,26 @@ class HipChatHandler(AbstractHandler):
         LOGGER.debug("Handler (%s) %s", self.name, level)
 
         message = self.get_short(level, *args, **kwargs)
+
+        if level in self.notify_levels:
+            message = '@here ' + message
+
         data = {
             'room_id': self.room,
-            'from': self.prefix,
+            'from': self.author,
             'message': message,
             'notify': 1,
             'color': self.colors.get(level, 'blue'),
             'message_format': 'text',
         }
         body = urllib.urlencode(data)
+
+        url = 'https://api.hipchat.com/v1/rooms/message?auth_token=' + self.key
+
+        LOGGER.debug("Request %s %s", url, body)
+
         yield self.client.fetch(
-            'https://api.hipchat.com/v1/rooms/message?auth_token=' + self.key,
-            method='POST', body=body)
+            url,
+            method='POST', body=body,
+            headers={'Content-Type': 'application/x-www-form-urlencoded'}
+        )
