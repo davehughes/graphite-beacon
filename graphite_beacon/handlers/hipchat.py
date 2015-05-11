0143f5b0a3bf5d8ff73f04e3a0feb8062d06be81
David Hughes <davehughes@uber.com>
2015-05-11 11:50:01 -0700

    Changed hipchat handler to target API versions 1 and 2 and handle at-mention notifications

--- graphite_beacon/handlers/hipchat.py
+++ graphite_beacon/handlers/hipchat.py
@@ -7,12 +7,14 @@ from . import AbstractHandler, LOGGER
 class HipChatHandler(AbstractHandler):
 
     name = 'hipchat'
+    SUPPORTED_API_VERSIONS = [1, 2]
 
     # Default options
     defaults = {
         'url': 'https://api.hipchat.com',
         'room': None,
         'key': None,
+        'api_version': 2,
     }
 
     colors = {
@@ -24,21 +26,61 @@ class HipChatHandler(AbstractHandler):
     def init_handler(self):
         self.room = self.options.get('room')
         self.key = self.options.get('key')
+        self.api_version = self.options.get('api_version', 2)
         assert self.room, 'Hipchat room is not defined.'
         assert self.key, 'Hipchat key is not defined.'
+        assert (self.api_version in self.SUPPORTED_API_VERSIONS,
+            'Hipchat API version {} is not supported'.format(self.api_version))
         self.client = hc.AsyncHTTPClient()
 
     @gen.coroutine
     def notify(self, level, *args, **kwargs):
         LOGGER.debug("Handler (%s) %s", self.name, level)
 
-        data = {
-            'message': self.get_short(level, *args, **kwargs).decode('UTF-8'),
-            'notify': True,
-            'color': self.colors.get(level, 'gray'),
-            'message_format': 'text',
-        }
+        message = self.get_short(level, *args, **kwargs).decode('UTF-8')
+        notify_levels = self.options.get('notify_levels', [])
+        if level in notify_levels:
+            message += ' ' + ' '.join('@' + name for name in notify_levels[level])
 
-        yield self.client.fetch('{url}/v2/room/{room}/notification?auth_token={token}'.format(
-            url=self.options.url, room=self.room, token=self.key),
-            headers={'Content-Type': 'application/json'}, method='POST', body=json.dumps(data))
+        if self.api_version == 1:
+            data = {
+                'message': message,
+                'color': self.colors.get(level, 'gray'),
+                'message_format': 'text',
+                'notify': 1,
+                'room_id': self.room,
+                'from': self.options.get('author', 'Graphite Beacon'),
+            }
+            url = '{url}/v1/rooms/message?auth_token={token}'.format(
+                    url=self.options.url,
+                    token=self.key,
+                    )
+            yield self.client.fetch(
+                url,
+                method='POST',
+                headers={'Content-Type': 'application/x-www-form-urlencoded'},
+                body=body,
+            )
+
+        elif self.api_version == 2:
+            data = {
+                'message': message,
+                'color': self.colors.get(level, 'gray'),
+                'message_format': 'text',
+                'notify': True,
+            }
+            url = '{url}/v2/room/{room}/notification?auth_token={token}'.format(
+                    url=self.options.url,
+                    room=self.room,
+                    token=self.key,
+                    )
+            yield self.client.fetch(
+                url=url,
+                method='POST',
+                headers={'Content-Type': 'application/json'},
+                body=json.dumps(data)
+            )
+
+        else:
+            LOGGER.error('Unsupported Hipchat API version: {}'
+                         .format(self.api_version))
